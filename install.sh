ccf71989f3eb3cecf398a986797ee0bad9ac21da
Aleksandar Pavić <acosonic@gmail.com>
2019-05-21 12:54:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "automated nano syntax install"
 wget https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh -O- | sh
+
+echo "set tabsize 4\nset tabstospaces" > ~/.nanorc
